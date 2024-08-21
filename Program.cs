bfe2360ffc289fa1d30ce1f8802685767d5430f4
Etim Israel <israeletim4@gmail.com>
2024-08-21 04:00:05 +0100

    Rechecking my code

--- Program.cs
+++ Program.cs
@@ -1,18 +1,21 @@
 ﻿// Counter
-// for (var i = 0; i < 10; i++)
-// {
-//   Console.WriteLine(i);
-// }
+for (var i = 0; i < 10; i++)
+{
+  Console.WriteLine(i);
+}
+Console.WriteLine("\n");
 
-// for (var i = 0; i <= 10; i++)
-// {
-//   Console.WriteLine(i);
-// }
+for (var i = 0; i <= 10; i++)
+{
+  Console.WriteLine(i);
+}
+Console.WriteLine("\n");
 
-// for (var i = 1; i <= 10; i++)
-// {
-//   Console.WriteLine(i);
-// }
+for (var i = 1; i <= 10; i++)
+{
+  Console.WriteLine(i);
+}
+Console.WriteLine("\n");
 
 string[] bestOrchestra = ["HOWASOM", "MUSON", "ELSH"];
 // to iterate through using the conventional method
@@ -20,15 +23,18 @@ for(var i = 0; i < bestOrchestra.Length; i++)
 {
   Console.WriteLine(bestOrchestra[i]);
 }
+Console.WriteLine("\n");
 // Another way to iterate is using foreach
 foreach (var item in bestOrchestra)
 {
   Console.WriteLine(item);
 }
+Console.WriteLine("\n");
 // A better way to iterate is using LINQ
 bestOrchestra.ToList().ForEach((i) => {
   Console.WriteLine("ForEach through: " + i);
 });
+Console.WriteLine("\n");
 
 // Another way that will actually act on Array
 Array.ForEach(bestOrchestra, e => Console.WriteLine("Array.ForEach: " + e));
--- obj/Debug/net8.0/forLoopCRUD.AssemblyInfo.cs
+++ obj/Debug/net8.0/forLoopCRUD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("forLoopCRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39a1c396339faf819c0f1a587657a9efaf9b8c1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("forLoopCRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("forLoopCRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
